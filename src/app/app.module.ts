acaf1ce158a8f433122e8f160b454ce9baf72fd3
fatemefazli <fatemefazli.bv@gmail.com>
2018-08-09 23:21:51 +0430

    add material to project.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { AppMaterialModules } from './material.module';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppMaterialModules
   ],
   providers: [],
   bootstrap: [AppComponent]
