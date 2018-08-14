8b4a30754f2bcc5353a11358796c4b39fd04aca1
fatemefazli <fatemefazli.bv@gmail.com>
2018-08-14 12:38:04 +0430

    add some code(list github organization repos).

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 
@@ -9,16 +11,20 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { SearchRepositoryService } from './github-repositories/search-repository.service';
 import { SearchRepositoryComponent } from './github-repositories/search-repository.component';
-import {HttpClientModule} from "@angular/common/http";
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 
 @NgModule({
   declarations: [
     AppComponent ,
-    SearchRepositoryComponent
+    SearchRepositoryComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
     HttpClientModule,
     AppMaterialModules,
     environment.production ? ServiceWorkerModule.register('ngsw-worker.js') : []
--- src/app/github-repositories/search-repository.component.ts
+++ src/app/github-repositories/search-repository.component.ts
@@ -1,22 +1,38 @@
-import { Component } from '@angular/core';
-import {SearchRepositoryService} from "./search-repository.service";
+import {Component, OnInit} from '@angular/core';
+import {SearchRepositoryService} from './search-repository.service';
+import {MatSnackBar} from '@angular/material';
 
 @Component({
   selector: 'app-search-repository',
   templateUrl: './search-repository.component.html',
   styleUrls: ['./search-repository.component.css']
 })
-export class SearchRepositoryComponent {
+export class SearchRepositoryComponent implements OnInit{
 
-  public repositories :any[] = [];
-  constructor(private  searchRepositoryService:SearchRepositoryService){}
+  public repositories: any[] = [];
+  public orgName = '';
+  constructor(private  searchRepositoryService: SearchRepositoryService,
+              public snackBar: MatSnackBar) {}
 
-  ngOnInit(){
-    this.searchRepositoryService.listOrganizationRepositories('angular').subscribe(data => {
+  ngOnInit() {}
+
+  listRepositories(organizationName: string) {
+    console.log('org name' , organizationName)
+    this.searchRepositoryService.listOrganizationRepositories(organizationName).subscribe(data => {
+      this.repositories = [];
       console.log(data);
       for(let item in data){
         this.repositories.push(data[item]);
       }
+      console.log(this.repositories);
+    },error => {
+      this.openSnackBar('Found no organization with this name', 'Try another')
     });
+  }
+
+  openSnackBar(message:string, action: string) {
+  this.snackBar.open(message, action, {
+    duration: 2000,
+   });
   }
 }
